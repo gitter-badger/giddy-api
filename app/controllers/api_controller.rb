7a6f49e03024160166c6b6b59e2cb31aa522bf0e
robikovacs <robi@wolfpack-digital.com>
2017-10-29 18:37:06 +0200

    Code style improvements

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Api::V1::BaseController < ActionController::API
+class ApiController < ActionController::API
   include Knock::Authenticable
   include Response
   include ExceptionHandler
