f35c2fd6f4026cbf774ca89425eefa9428f6f683
Robert Kovacs <robi.kovacs11@gmail.com>
2018-05-31 23:02:50 +0300

    Change db name

--- postgres-initdb.sh
+++ postgres-initdb.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE DATABASE "dev";
+  CREATE DATABASE "giddy-dev";
 EOSQL
 
-psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=dev <<-EOSQL
+psql --variable=ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=giddy-dev <<-EOSQL
   CREATE EXTENSION "uuid-ossp";
   CREATE EXTENSION "hstore";
 EOSQL
